e3f9dcea3e228bcf6abbfac4a87ccd7a826b4291
ramyagopalk <ramyagopalk@gmail.com>
2014-07-31 12:47:45 +0530

    2nd

---
+++ Accolite.GeekWeek.Voting/Controllers/VotingDashboardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Accolite.GeekWeek.Voting.Controllers
+{
+    public class VotingDashboardController : Controller
+    {
+        //
+        // GET: /VotingDashboard/
+
+        public ActionResult Landing()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Accolite.GeekWeek.Voting/Models/VotingDashboardModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Accolite.GeekWeek.Voting.Models
+{
+    public class VotingDashboardModel
+    {
+    }
+}
