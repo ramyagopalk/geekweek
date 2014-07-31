3198e7e4a59b4f426424526105cd23cc9ea1e681
ramyagopalk <ramyagopalk@gmail.com>
2014-07-31 17:59:31 +0530

    4th

--- Accolite.GeekWeek.Voting/Content/bootstrap-star-rating/js/star-rating.js
+++ Accolite.GeekWeek.Voting/Content/bootstrap-star-rating/js/star-rating.js
@@ -353,16 +353,16 @@
         showCaption: true,
         defaultCaption: '{rating} Stars',
         starCaptions: {
-            0.5: 'Half Star',
-            1: 'One Star',
-            1.5: 'One & Half Star',
+            0.5: 'AAG!!',
+            1: 'Disappointed',
+            1.5: 'Bad',
             2: 'Two Stars',
-            2.5: 'Two & Half Stars',
-            3: 'Three Stars',
+            2.5: 'Pass',
+            3: 'Can do better',
             3.5: 'Three & Half Stars',
-            4: 'Four Stars',
-            4.5: 'Four & Half Stars',
-            5: 'Five Stars'
+            4: 'Yahoo',
+            4.5: 'Just miss',
+            5: 'WHOA!!'
         },
         starCaptionClasses: {
             0.5: 'label label-danger',
